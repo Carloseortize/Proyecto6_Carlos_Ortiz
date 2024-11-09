c3563f6a0aec4891ad18ea0354becde61a113892
Carlos Ortiz <carloseortize@hotmail.com>
2024-11-08 21:26:09 -0500

    Envios de correcciones_2

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ if build_histogram:  # Si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
 
     # Generar el histograma usando Plotly Express
-    fig = px.histogram(car_data, x='odometer',
+    fig = px.histogram(car_data, x='Odometer',
                        title="Histograma de Odómetro")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
 build_diagrama = st.checkbox(
-    'Construir Diagrama de Dispersión')  # crear un botón
+    'Construir Diagrama de Dispersión')  # crear una casilla de verificación
 
 if build_diagrama:  # Si la casilla de verificación está seleccionada
     st.write('Creación de un Diagrama de Dispersión para el conjunto de datos de anuncios de venta de coches')
 
     # crear un diagrama de dispersión
-    fig = px.scatter(car_data, x="Odometer", y="Price",
+    fig = px.scatter(car_data, x="odometer", y="price",
                      title="Diagrama de Dispersión de Odómetro vs. Precio")
 
     # mostrar un gráfico Plotly interactivo
